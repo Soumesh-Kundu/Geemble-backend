df3e4ab3adbfcd9f2db76c38a401f00af46df75d
Soumesh <107293904+Soumesh-Kundu@users.noreply.github.com>
2024-05-14 03:19:16 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app=express()
 const PORT=process.env.PORT ?? 5000
 connectToDB()
 
-app.use(cors())
+app.use(cors({origin:"https://geemble.vercel.app/"}))
 app.use(cookieParser())
 app.use(express.json())
 InitFirebase()
