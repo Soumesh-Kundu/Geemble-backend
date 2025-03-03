14d6db0c7d32ca5d90257da1dc617c312523b04d
Soumesh Kundu <iamsoumo26@gmail.com>
2025-03-04 03:14:55 +0530

    v2: Geemble Backend

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app=express()
 const PORT=process.env.PORT ?? 5000
 connectToDB()
 
-app.use(cors({origin:process.env.FRONTEND_URL}))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 InitFirebase()
